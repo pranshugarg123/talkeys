1b8b5177e4450f41277518c2f9f6db8481518968
himanishpuri <himanishpuri2203@gmail.com>
2025-02-01 14:10:58 +0530

    minor

--- Client/src/app/layout.tsx
+++ Client/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import image from "@/public/images/bg.png";
+import image from "@/public/images/bg.webp";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Analytics } from "@vercel/analytics/react";
