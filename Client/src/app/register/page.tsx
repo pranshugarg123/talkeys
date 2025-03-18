ae0479edaf3704caa032c6c6b06b6c8ee0f7bbc1
Riteshk1314 <riteshkapoor1314@gmail.com>
2025-03-18 23:49:32 +0530

    Update page.tsx

--- Client/src/app/register/page.tsx
+++ Client/src/app/register/page.tsx
@@ -95,6 +95,7 @@ export default function RegisterPage() {
     try {
       const token = localStorage.getItem("accessToken");
       console.log("Submitting form with data:", data);
+      console.log("Token:", token);
       const response = await fetch(`https://api.talkeys.xyz/register`, {
         method: "POST",
         headers: {
