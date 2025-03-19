c3a4acd4b0b06fa28959810b85921231bb10691b
himanishpuri <himanishpuri2203@gmail.com>
2025-03-19 11:42:22 +0530

    client folder removed

--- Client/client/src/app/admin/addEvent/page.tsx
+++
@@ -1,9 +0,0 @@
-import AddEventPage from "@/components/admin/AddEventPage";
-
-export default function AddEventPageWrapper() {
-	return (
-		<div className="min-h-screen pt-10 bg-gray-950/35 text-white">
-			<AddEventPage />
-		</div>
-	);
-}
--- Client/client/src/app/admin/page.tsx
+++
@@ -1,9 +0,0 @@
-import AdminDashboard from "@/components/admin/AdminDashboard";
-
-export default function AdminDashboardPage() {
-	return (
-		<div className="min-h-screen bg-gray-950 text-white p-4">
-			<AdminDashboard />
-		</div>
-	);
-}
